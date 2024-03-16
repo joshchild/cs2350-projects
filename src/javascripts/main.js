0d169df1be92232499c9b3cb8602bb7802af8951
Josh Child <joshchild@Joshs-MacBook-Pro-2.local>
2024-03-15 22:09:03 -0600

    a6 test commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,12 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+// ================== Assignment 6 ================== 
+//getMax(x, y, z): This function takes three numbers, x, y, and z, as input and returns the largest among them.
+function getMax(x, y, z, t) {
+    return Math.max(x, y, z, t);
+}
+console.log(getMax(5, 10, 3)); // example output: 10
+
+//getMin(x, y, z): The opposite of max(x, y, z). It returns the smallest among x, y, and z.
+
